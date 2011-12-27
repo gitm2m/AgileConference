4fca0893b1cd8a70b551052568751c4d9a96f462
Deepak Shukla <deepak.shukla@valtech.co.in>
2011-12-27 12:35:11 +0530

    Added folder architecture

--- AgileConference/AppController/ACAppControler.h
+++ AgileConference/AppController/ACAppControler.h
@@ -1,13 +1,13 @@
 //
-//  ACViewController.h
+//  ACAppControler.h
 //  AgileConference
 //
-//  Created by Valtech India Systems Pvt Ltd on 26/12/11.
+//  Created by Valtech India Systems Pvt Ltd on 27/12/11.
 //  Copyright (c) 2011 deepak.shukla@valtech.co.in. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ACViewController : UIViewController
+@interface ACAppControler : UIViewController
 
 @end
---
+++ AgileConference/AppController/ACAppControler.m
@@ -0,0 +1,66 @@
+//
+//  ACAppControler.m
+//  AgileConference
+//
+//  Created by Valtech India Systems Pvt Ltd on 27/12/11.
+//  Copyright (c) 2011 deepak.shukla@valtech.co.in. All rights reserved.
+//
+
+#import "ACAppControler.h"
+
+@implementation ACAppControler
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UILabel *label =[[UILabel alloc] initWithFrame:CGRectMake(0, 0,100, 130)];
+    [self.view addSubview:label];
+    [self.view setBackgroundColor:[UIColor redColor]];
+    [label setText:@"11178234762378462783648723>>>>>>>>>>>>>>>"];
+
+}
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
