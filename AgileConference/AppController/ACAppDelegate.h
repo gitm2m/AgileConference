2b066d96bc6331a5ad438ca36cd1db2f9b4c4a01
Deepak Shukla <deepak.shukla@valtech.co.in>
2011-12-27 12:42:56 +0530

    Added initial files

---
+++ AgileConference/AppController/ACAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  ACAppDelegate.h
+//  AgileConference
+//
+//  Created by Valtech India Systems Pvt Ltd on 26/12/11.
+//  Copyright (c) 2011 deepak.shukla@valtech.co.in. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ACViewController.h"
+#import "ACAppControler.h"
+
+
+@interface ACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AgileConference/AppController/ACViewController.h
@@ -0,0 +1,13 @@
+//
+//  ACViewController.h
+//  AgileConference
+//
+//  Created by Valtech India Systems Pvt Ltd on 26/12/11.
+//  Copyright (c) 2011 deepak.shukla@valtech.co.in. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACViewController : UIViewController
+
+@end
