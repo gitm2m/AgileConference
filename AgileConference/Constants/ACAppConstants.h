c60c579cef056acc7ab1238358f1c15620558f1b
Deepak Shukla <deepak.shukla@valtech.co.in>
2011-12-28 12:19:51 +0530

    added constants and other file

---
+++ AgileConference/Constants/ACAppConstants.h
@@ -0,0 +1,13 @@
+//
+//  ACAppConstants.h
+//  AgileConference
+//
+//  Created by Supreeth Doddabela on 12/28/11.
+//  Copyright (c) 2011 Valtech India. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACAppConstants 
+
+#define KAppName @"AgileConference"
