cd8873d33eb5972371b55447e2d60125fa8022d4
Deepak Shukla <deepak.shukla@valtech.co.in>
2012-02-04 00:28:36 +0530

    app id added

--- AgileConference/SupportingLibraries/AppstoreFeedback/Appirater.h
+++ AgileConference/SupportingLibraries/AppstoreFeedback/Appirater.h
@@ -46,7 +46,7 @@ extern NSString *const kAppiraterDeclinedToRate;
 /*
  Place your Apple generated software id here.
  */
-#define APPIRATER_APP_ID				373431224
+#define APPIRATER_APP_ID				500208032
 
 /*
  Your app's name.
